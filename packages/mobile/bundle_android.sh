dcc8890a011e5863032913456ad7b6fe639fa85f
Felix Limanta <felix@ks.c.titech.ac.jp>
2022-10-21 15:47:22 +0900

    Updated package.json

---
+++ packages/mobile/bundle_android.sh
@@ -0,0 +1,6 @@
+mkdir -p android/app/src/main/assets
+react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res/
+
+rm -rf ./android/app/src/main/res/drawable-*
+rm -rf ./android/app/src/main/res/raw
+
